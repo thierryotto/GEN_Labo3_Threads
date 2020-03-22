6265f4e362032bcb3b1ef9fa88bd07edb2153e96
Thierry Otto <thierry.otto@heig-vd.ch>
2020-03-22 20:38:25 +0100

    Exercice banque partie 1

---
+++ banque/src/main/java/Transferts.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+/**
+ * Classe exécutant N transferts entre comptes dans son propre thread.
+ * Les comptes et les montants de chaque transfert sont tirés aléatoirement.
+ */
+public class Transferts implements Runnable {
+    private int nbComptes = 0;
+    private int nbTransferts = 0;
+    private Banque banque;
+
+    /**
+     * Constructeur
+     */
+    public Transferts(int nbComptes, int nbTransferts) {
+        this.nbComptes = nbComptes;
+        this.banque = new Banque(nbComptes);
+        this.nbTransferts = nbTransferts;
+    }
+
+    /**
+     * Retourne la valeur du
+     *
+     * @return vrai si les comptes sont consistant, faux sinon.
+     */
+    public boolean getCheckConsistent() {
+        return banque.consistent();
+    }
+
+    /**
+     * Méthode qui est exécuté au lancement du thread
+     */
+    public void run() {
+        final int SOMMEMAX = 100;
+
+        // Faire un certain nombre de transfert
+        // Les comptes et les montants de chaque transfert sont tirés aléatoirement.
+        for (int i = 0; i < nbTransferts; i++) {
+            banque.transfert(Util.random.nextInt(this.nbComptes),
+                             Util.random.nextInt(this.nbComptes),
+                             Util.random.nextInt(SOMMEMAX));
+
+            // 1sec = 1000 millis
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Méthode main pour un test
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        final int NBCOMPTES = 10;
+        final int NBTRANSFERTS = 100;
+
+        Transferts t1 = new Transferts(NBCOMPTES, NBTRANSFERTS);
+        Thread thread1 = new Thread(t1);
+        thread1.start();
+        thread1.join();
+
+        System.out.println(t1.getCheckConsistent());
+    }
+}
+
+class Util {
+    public static final Random random = new Random(0x42);
+}
---
+++ banque/src/test/java/TransfertsTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransfertsTest {
+
+    @Test
+    void declencher1000TransfertsBanque10Comptes() throws InterruptedException {
+        final int NBCOMPTES = 10;
+        final int NBTRANSFERTS = 100;
+        final int NBBOUCLES = 10;
+
+        Transferts t1 = new Transferts(NBCOMPTES, NBTRANSFERTS);
+        LinkedList<Thread> threadsTransfert = new LinkedList<>();
+
+        // Exécuter N fois la classe de transfert
+        for (int i = 0; i < NBBOUCLES; i++) {
+            threadsTransfert.add(new Thread(t1));
+            threadsTransfert.get(i).start();
+        }
+
+        // Attendre sur la fin de l'exécution de tous les threads
+        for (int i = 0; i < NBBOUCLES; i++) {
+            threadsTransfert.get(i).join();
+        }
+
+        // Vérifier que les comptes de la banques sont consistents
+        assertTrue(t1.getCheckConsistent());
+    }
+}
